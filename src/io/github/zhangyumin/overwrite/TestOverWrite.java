328245bfa51359d19cfa5bd643480a12591f3bfc
zhangyumin <zhangyumin618@163.com>
2015-07-21 20:26:50 +0800

    overwrite

---
+++ src/io/github/zhangyumin/overwrite/TestOverWrite.java
@@ -0,0 +1,62 @@
+package io.github.zhangyumin.overwrite;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-7-21.
+ */
+class Person{
+    private String name;
+    public int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getInfo(){
+        return "Name: "+ name +"\n"+ "age: "+age;
+    }
+}
+class Student extends Person{
+    private String school;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    public String getInfo(){
+        return "Name: "+ getName() +"\nage: "+getAge()+"\nschool: "+school;
+    }
+}
+public class TestOverWrite {
+    public static void main(String arg[]){
+        Student student = new Student();
+        Person person = new Person();
+        person.setName("Tom");
+        person.setAge(24);
+        student.setName("Jerry");
+        student.setAge(18);
+        student.setSchool("XMU");
+        System.out.println(person.getInfo());
+        System.out.println(student.getInfo());
+    }
+}
