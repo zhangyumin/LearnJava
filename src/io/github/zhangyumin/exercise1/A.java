7bd5cac3ea8008659a691d7852edb279f4b92f9d
zhangyumin <zhangyumin618@163.com>
2015-07-21 21:20:08 +0800

    exercise1

---
+++ src/io/github/zhangyumin/exercise1/A.java
@@ -0,0 +1,35 @@
+package io.github.zhangyumin.exercise1;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-7-21.
+ */
+class A {
+    protected void print(String s){
+        System.out.println(s);
+    }
+    A(){
+        print("A()");
+    }
+    public void f(){
+        print("A:f()");
+    }
+}
+class B extends A{
+    B(){
+        print("B()");
+    }
+    public void f() {
+        print("B:f()");
+    }
+    public static void main(String[] args){
+        B b = new B();
+        b.f();
+    }
+}
