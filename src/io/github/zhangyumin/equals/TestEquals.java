8b21a075000962aaea84f660433a42b7c19686db
zhangyumin <zhangyumin618@163.com>
2015-07-22 11:34:34 +0800

    lang.equals

---
+++ src/io/github/zhangyumin/equals/TestEquals.java
@@ -0,0 +1,41 @@
+package io.github.zhangyumin.equals;
+
+import java.util.Objects;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-7-22.
+ */
+public class TestEquals {
+    public static void main(String[] args){
+        Cat c1 = new Cat(1,2,3);
+        Cat c2 = new Cat(1,2,3);
+        System.out.println(c1.equals(c2));
+    }
+}
+class Cat{
+    int color;
+    int height,weight;
+    public Cat(int color,int height,int weight){
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+    }
+    public boolean equals(Object obj){
+        if(obj == null)
+            return false;
+        else if (obj instanceof Cat){
+            Cat c = (Cat) obj;
+            if(c.color == this.color && c.height == this.height && c.weight == this.weight){
+                return true;
+            }
+        }
+            return false;
+    }
+}
