be717e80001591c85f5f3488b788aee3af588f57
zhangyumin <zhangyumin618@163.com>
2015-10-16 11:24:14 +0800

    TestArgsWords

---
+++ src/TestArgsWords.java
@@ -0,0 +1,25 @@
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-10-16.
+ */
+import java.util.*;
+import java.util.HashMap;
+
+public class TestArgsWords {
+    private static final Integer ONE = new Integer(1);
+    public static void main(String args[]){
+        Map m = new HashMap();
+        for(int i=0;i<args.length;i++){
+            Integer freq = (Integer)m.get(args[i]);
+            m.put(args[i],(freq == null ? ONE : new Integer(freq.intValue()+1)));
+        }
+        System.out.println(m.size()+" distinct words detected:");
+        System.out.println(m);
+    }
+}
