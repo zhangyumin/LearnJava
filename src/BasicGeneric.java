6f28cdf83b5e5646ead3f6c280c86d0571e18cfb
zhangyumin <zhangyumin618@163.com>
2015-10-16 16:41:18 +0800

    BasicGeneric

---
+++ src/BasicGeneric.java
@@ -0,0 +1,44 @@
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-10-16.
+ */
+import java.util.*;
+import java.util.HashSet;
+
+public class BasicGeneric {
+    public static void main(String args[]){
+        List<String> c = new ArrayList<String>();
+        c.add("aaa");
+        c.add("bbb");
+        c.add("ccc");
+        for(int i=0; i<c.size(); i++){
+            String s = c.get(i);
+            System.out.println(s);
+        }
+        Collection<String> c2 = new HashSet<String>();
+        c2.add("aaa");
+        c2.add("bbb");
+        c2.add("ccc");
+        for(Iterator<String> it = c2.iterator(); it.hasNext();){
+            String s = it.next();
+            System.out.println(s);
+        }
+    }
+}
+class Myname implements Comparable<Myname>{
+    int age;
+    public int compareTo(Myname mn){
+        if(this.age > mn.age)
+            return 1;
+        else if(this.age < mn.age)
+            return -1;
+        else
+            return 0;
+    }
+}
