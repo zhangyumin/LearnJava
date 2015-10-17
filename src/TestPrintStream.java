a21aa85c925651a231f3d893d7076c79903c452f
zhangyumin <zhangyumin618@163.com>
2015-10-17 14:13:11 +0800

    TestPrintStream

---
+++ src/TestPrintStream.java
@@ -0,0 +1,35 @@
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-10-17.
+ */
+import java.util.*;
+import java.io.*;
+public class TestPrintStream {
+    public static void main(String args[]){
+        String s = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            FileWriter fw = new FileWriter("/home/zym/IdeaProjects/LearningJava/src/log.txt",true);
+            PrintWriter log = new PrintWriter(fw);
+            while((s=br.readLine())!=null){
+                if(s.equalsIgnoreCase("exit"))
+                    break;
+                System.out.println(s.toUpperCase());
+                log.println("-----");
+                log.println(s.toUpperCase());
+                log.flush();
+            }
+            log.println("==="+new Date()+"===");
+            log.flush();
+            log.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
