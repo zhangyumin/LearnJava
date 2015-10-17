40c25f6559c6b443342e60401ac096b4e8a72b5d
zhangyumin <zhangyumin618@163.com>
2015-10-17 19:41:39 +0800

    DeadLock

---
+++ src/DeadLock.java
@@ -0,0 +1,54 @@
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-10-17.
+ */
+public class DeadLock implements Runnable {
+    public int flag = 1;
+    static Object o1 = new Object(), o2 = new Object();
+
+    public void run() {
+        System.out.println("flag=" + flag);
+        if (flag == 1) {
+            synchronized (o1) {
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                synchronized (o2) {
+                    System.out.println("1");
+                }
+            }
+        }
+        if (flag == 0) {
+            synchronized (o2) {
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (o1) {
+                    System.out.println("0");
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        DeadLock dl1 = new DeadLock();
+        DeadLock dl2 = new DeadLock();
+        dl1.flag = 1;
+        dl2.flag = 0;
+        Thread t1 = new Thread(dl1);
+        Thread t2 = new Thread(dl2);
+        t1.start();
+        t2.start();
+    }
+}
