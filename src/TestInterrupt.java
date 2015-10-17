cbf20ebc01d77f37420d838b6ca92610a9d00f07
zhangyumin <zhangyumin618@163.com>
2015-10-17 15:35:21 +0800

    TestInterrupt

---
+++ src/TestInterrupt.java
@@ -0,0 +1,35 @@
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-10-17.
+ */
+import java.util.*;
+public class TestInterrupt {
+    public static void main(String args[]){
+        MyThread thread =new MyThread();
+        thread.start();
+        try {
+            Thread.sleep(10000);
+        }catch (InterruptedException e){
+
+        }
+        thread.interrupt();
+    }
+}
+class MyThread extends Thread{
+    public void run(){
+        while(true){
+            System.out.println("==="+new Date()+"===");
+            try {
+                sleep(1000);
+            }catch (InterruptedException e){
+                return;
+            }
+        }
+    }
+}
