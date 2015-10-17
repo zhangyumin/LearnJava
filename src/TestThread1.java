c1951d76edd3bfb823adc2fa20f6d0d9528b26f8
zhangyumin <zhangyumin618@163.com>
2015-10-17 15:35:04 +0800

    TestThread1

---
+++ src/TestThread1.java
@@ -0,0 +1,30 @@
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-10-17.
+ */
+public class TestThread1 {
+    public static void main(String args[]){
+        Runner1 r = new Runner1();
+        r.start();
+//        r.run();
+//        Thread t = new Thread(r);
+//        t.start();
+        for(int i=0; i<100; i++){
+            System.out.println("Main thread:-----------"+i);
+        }
+    }
+}
+//class Runner1 implements Runnable{
+class Runner1 extends Thread{
+    public void run(){
+        for(int i=0; i<100; i++){
+            System.out.println("Runner1:-----------"+i);
+        }
+    }
+}
