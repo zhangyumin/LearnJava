47014dc8395bdff7ad09a27c214baaeee68e6806
zym-workstation <zhangyumin618@163.com>
2015-11-17 10:29:51 +0800

    extends and abstract

---
+++ src/io/github/zhangyumin/People/TestPeople.java
@@ -0,0 +1,38 @@
+package io.github.zhangyumin.People;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-11-17.
+ */
+public class TestPeople {
+    public static void main(String[] args){
+        People p1 = new Chinese();
+        p1.speak();
+        People p2 = new American();
+        p2.speak();
+    }
+}
+
+abstract class People{
+    public abstract void speak();
+}
+
+class Chinese extends People{
+    @Override
+    public void speak() {
+        System.out.println("你好");
+    }
+}
+
+class American extends People{
+    @Override
+    public void speak() {
+        System.out.println("hello");
+    }
+}
