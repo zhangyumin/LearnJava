085eeb18d53a619fc17f43bfffb3f88016621d55
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-03 11:28:55 +0800

    fix bug

--- src/io/github/zhangyumin/CourseSelection/Student.java
+++ src/io/github/zhangyumin/CourseSelection/Student.java
@@ -1,4 +1,7 @@
-package io.github.zhangyumin.CarRental;
+package io.github.zhangyumin.CourseSelection;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * //
@@ -8,29 +11,15 @@ package io.github.zhangyumin.CarRental;
  * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
  * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
  * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
- * Created by zym on 15-11-17.
+ * Created by zym on 15-11-21.
  */
-public class Car {
-    int id;
-    String name;
-    int price;
-    int burdenPeople;
-    int burdenGood;
-    Car(int id,String name,int price,int burdenGood,int burdenPeople){
+public class Student {
+    public int id;
+    public String name;
+    public Set courses;
+    public Student(int id,String name){
         this.id = id;
         this.name = name;
-        this.price = price;
-        this.burdenPeople = burdenPeople;
-        this.burdenGood = burdenGood;
-        print();
-    }
-
-    void print(){
-        if(burdenGood>0 && burdenPeople>0)
-            System.out.println(id+" "+name+" "+price+"元/天"+" "+"载人:"+burdenPeople+"人 "+"载货:"+burdenGood+"吨");
-        else if(burdenGood>0 && burdenPeople<=0)
-            System.out.println(id+" "+name+" "+price+"元/天"+" "+"载货:"+burdenGood+"吨");
-        else if(burdenGood<=0 && burdenPeople>0)
-            System.out.println(id+" "+name+" "+price+"元/天"+" "+"载人:"+burdenPeople+"人");
+        this. courses = new HashSet();
     }
 }
