3b960d7dfbca9946e258cce9c7be5f73dee860ba
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-10 17:16:17 +0800

    InetAddress

---
+++ src/io/github/zhangyumin/TestInetAddress/InetAddressTest.java
@@ -0,0 +1,31 @@
+package io.github.zhangyumin.TestInetAddress;
+import java.lang.reflect.Array;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-12-10.
+ */
+public class InetAddressTest {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = InetAddress.getLocalHost();
+        System.out.println(address.getHostName());
+        System.out.println(address.getHostAddress());
+        byte[] bytes = address.getAddress();
+        System.out.println(Arrays.toString(bytes));
+        System.out.println(address);
+
+        InetAddress address2 = InetAddress.getByName("127.0.0.1");
+        System.out.println(address2);
+
+    }
+
+}
