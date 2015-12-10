ec139a7f928c45606a1665c75f7e27fb478622ff
zhangyumin618@163.com <zhangyumin618@163.com>
2015-12-10 22:14:59 +0800

    Url test

---
+++ src/io/github/zhangyumin/TestUrl/TestUrl.java
@@ -0,0 +1,41 @@
+package io.github.zhangyumin.TestUrl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 15-12-10.
+ */
+public class TestUrl {
+    public static void main(String[] args) {
+        try {
+            URL paspa = new URL("http://bmi.xmu.edu.cn/paspa");
+            InputStream is = paspa.openStream();
+            InputStreamReader isr = new InputStreamReader(is,"utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String content = br.readLine();
+            while(content != null){
+                System.out.println(content);
+                content = br.readLine();
+            }
+            br.close();
+            isr.close();
+            is.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
