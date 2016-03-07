4ded270125b43d41f95d5446efe0f1e9ad9dce36
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-07 21:15:58 +0800

    reflection

---
+++ src/book/core/java/ObjectAnalyzer/ObjectAnalyzerTest.java
@@ -0,0 +1,25 @@
+package book.core.java.ObjectAnalyzer;
+
+import java.util.ArrayList;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-7.
+ */
+public class ObjectAnalyzerTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> squares = new ArrayList();
+        for (int i = 0; i <= 5; i++) {
+            squares.add(i * i);
+        }
+        System.out.println(new ObjectAnalyzer().toString(squares));
+    }
+}
+
+
