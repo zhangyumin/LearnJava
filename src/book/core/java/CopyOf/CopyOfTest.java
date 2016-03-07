689c67da7bad2ed2e0fdafb5004709f2979e7f05
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-07 21:33:16 +0800

    reflection

---
+++ src/book/core/java/CopyOf/CopyOfTest.java
@@ -0,0 +1,48 @@
+package book.core.java.CopyOf;
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-7.
+ */
+public class CopyOfTest {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        a = (int[]) goodCopyOf(a, 10);
+        System.out.println(Arrays.toString(a));
+
+        String[] b = {"Tom", "Dick", "Mary"};
+        b = (String[]) goodCopyOf(b, 10);
+        System.out.println(Arrays.toString(b));
+
+        System.out.println("The following call will generate an exception.");
+        b = (String[]) badCopyOf(b, 10);
+    }
+    public static Object[] badCopyOf(Object[] a, int newLength){
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(a, 0, newArray, 0, Math.min(a.length, newLength));
+        return newArray;
+    }
+
+    public static Object goodCopyOf(Object a,int newLength){
+        Class cl = a.getClass();
+        if(!cl.isArray()){
+            return null;
+        }
+        Class componentType = cl.getComponentType();
+        int length = Array.getLength(a);
+        Object newArray = Array.newInstance(componentType, newLength);
+        System.arraycopy(a, 0 ,newArray, 0, Math.min(length, newLength));
+        return newArray;
+    }
+}
+
