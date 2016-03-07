9fdbcab2936ba2a9175f22c19aaadb4919d5cf2f
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-07 22:14:35 +0800

    reflection

---
+++ src/book/core/java/Methods/MethodPointerTest.java
@@ -0,0 +1,42 @@
+package book.core.java.Methods;
+
+import java.lang.reflect.Method;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-7.
+ */
+public class MethodPointerTest {
+    public static void main(String[] args) throws Exception{
+        Method square = MethodPointerTest.class.getMethod("square", double.class);
+        Method sqrt = Math.class.getMethod("sqrt", double.class);
+
+        printTable(1, 10, 10, square);
+        printTable(1, 10, 10, sqrt);
+    }
+
+    public static double square(double x){
+        return x * x;
+    }
+
+    public static void printTable(double from, double to, int n, Method f){
+        System.out.println(f);
+
+        double dx = (to - from) / (n - 1);
+
+        for(double x = from; x<= to; x += dx){
+            try {
+                double y = (Double) f.invoke(null, x);
+                System.out.printf("%10.4f | %10.4f%n", x, y);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
