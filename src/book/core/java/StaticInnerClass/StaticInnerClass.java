6f8ac068a5bac9770e6ee786e3f4f24884eb5e6d
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-08 20:44:46 +0800

    static inner class

---
+++ src/book/core/java/StaticInnerClass/StaticInnerClass.java
@@ -0,0 +1,55 @@
+package book.core.java.StaticInnerClass;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-8.
+ */
+public class StaticInnerClass {
+    public static void main(String[] args) {
+        double[] d = new double[20];
+        for (int i = 0; i < d.length; i++) {
+            d[i] = 100 * Math.random();
+        }
+        ArrayAlg.Pair p = ArrayAlg.minmax(d);
+        System.out.println("Min="+p.getFirst());
+        System.out.println("Max="+p.getSecond());
+    }
+}
+
+class ArrayAlg{
+    public static class Pair{
+        private double first;
+        private double second;
+
+        public Pair(double f, double s){
+            first = f;
+            second = s;
+        }
+
+        public double getFirst() {
+            return first;
+        }
+
+        public double getSecond() {
+            return second;
+        }
+    }
+
+    public static Pair minmax(double[] values){
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        for(double v : values){
+            if(min > v)
+                min = v;
+            if(max < v)
+                max = v;
+        }
+        return new Pair(min, max);
+    }
+}
