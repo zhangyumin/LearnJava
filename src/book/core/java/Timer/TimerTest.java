5b8c04422f90db611d88130149e0be5d78e91da0
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-08 16:25:13 +0800

    callback

---
+++ src/book/core/java/Timer/TimerTest.java
@@ -0,0 +1,39 @@
+package book.core.java.Timer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-8.
+ */
+public class TimerTest {
+    public static void main(String[] args) {
+        ActionListener listener = new TimePrinter();
+
+        Timer t = new Timer(10000, listener);
+        t.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
+
+class TimePrinter implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Date now = new Date();
+        System.out.println("At the tone, the time is "+ now);
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
