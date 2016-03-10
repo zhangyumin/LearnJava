4624b29e5d427e6d02149904e39cca6b9e975f41
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-10 16:48:53 +0800

    generic class

---
+++ src/book/core/java/CurrentModificationException.java
@@ -0,0 +1,30 @@
+package book.core.java;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-10.
+ */
+public class CurrentModificationException {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("world");
+        list.add("im");
+        list.add("joker");
+        Iterator iterator1 = list.iterator();
+        Iterator iterator2 = list.iterator();
+        iterator2.next();
+        iterator2.remove();
+        iterator1.next();
+    }
+}
