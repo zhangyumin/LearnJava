a188cb185d420bd9018b5e7d8097c396dc713d32
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-10 23:04:10 +0800

    set

---
+++ src/book/core/java/Set/SetTest.java
@@ -0,0 +1,39 @@
+package book.core.java.Set;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-10.
+ */
+public class SetTest {
+    public static void main(String[] args) {
+        Set<String> words = new HashSet<>();
+        long totalTime = 0;
+
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String word = in.next();
+            long callTime = System.currentTimeMillis();
+            words.add(word);
+            callTime = System.currentTimeMillis() - callTime;
+            totalTime += callTime;
+        }
+
+        Iterator<String> iter = words.iterator();
+        for (int i = 1; i <= 20 && iter.hasNext(); i++) {
+            System.out.println(iter.next());
+        }
+        System.out.println("...");
+        System.out.println(words.size() + " distinct words" + totalTime + " milliseconds");
+    }
+}
