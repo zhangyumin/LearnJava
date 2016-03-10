15e3e1c24b59902f16fb2fd4936c188642ef4736
zhangyumin618@163.com <zhangyumin618@163.com>
2016-03-10 22:41:20 +0800

    linkedlist

---
+++ src/book/core/java/LinkedList/LinkedListTest.java
@@ -0,0 +1,53 @@
+package book.core.java.LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-3-10.
+ */
+public class LinkedListTest {
+    public static void main(String[] args) {
+        List<String> a = new LinkedList<>();
+        a.add("Amy");
+        a.add("Carl");
+        a.add("Erica");
+
+        List<String> b = new LinkedList<>();
+        b.add("Bob");
+        b.add("Doug");
+        b.add("Frances");
+        b.add("Gloria");
+
+        ListIterator<String> aIter = a.listIterator();
+        ListIterator<String> bIter = b.listIterator();
+
+        while (bIter.hasNext()){
+            aIter.add(bIter.next());
+        }
+        System.out.println(a);
+
+        bIter = (ListIterator<String>) b.iterator();
+        while(bIter.hasNext()){
+            bIter.next();
+            if(bIter.hasNext()){
+                bIter.next();
+                bIter.remove();
+            }
+        }
+
+        System.out.println(b);
+
+        a.removeAll(b);
+
+        System.out.println(a);
+    }
+}
