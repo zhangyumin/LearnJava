5cd1610b771e30e2ff5690603e576bcb0e057199
zhangyumin618@163.com <zhangyumin618@163.com>
2016-05-05 22:41:45 +0800

    MovingZeros

---
+++ src/io/github/zhangyumin/MovingZeroes/MovingZeros.java
@@ -0,0 +1,31 @@
+package io.github.zhangyumin.MovingZeroes;
+
+import java.util.Arrays;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-5-5.
+ */
+public class MovingZeros {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        for (int i = 0; i < nums.length; i ++) {
+            if(nums[i] == 0) {
+                for (int j = i + 1; j < nums.length; j++){
+                    if(nums[j] != 0) {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
