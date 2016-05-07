62d6d0743b8cd33ebb6d005a4b1768e17475b636
zhangyumin <zhangyumin618@163.com>
2016-05-07 11:58:07 +0800

    MiniDuckSimulator version 1.0 in page 16

---
+++ src/book/designpattern/headfirst/MiniDuckSimulatorV1/MallardDuck.java
@@ -0,0 +1,18 @@
+package book.designpattern.headfirst.MiniDuckSimulatorV1;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by joker on 16-5-7.
+ */
+public class MallardDuck extends Duck {
+    public MallardDuck(){
+        flyBehaviour = new FlyWithWings();
+        quackBehaviour = new Squeak();
+    }
+}
