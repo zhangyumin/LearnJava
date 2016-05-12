05a92dfde4ce8276b6adccfd089f8a119b9e3d4b
zhangyumin618@163.com <zhangyumin618@163.com>
2016-05-12 16:45:39 +0800

    Unsynch Bank

---
+++ src/book/core/java/Unsynch/UnsynchBankTest.java
@@ -0,0 +1,25 @@
+package book.core.java.Unsynch;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-5-12.
+ */
+public class UnsynchBankTest {
+    public static final int NACCOUNT = 100;
+    public static final double INITIAL_BALANCE = 1000;
+
+    public static void main(String[] args) {
+        Bank b = new Bank(100, 1000);
+        for (int i = 0; i < NACCOUNT; i++) {
+            TransferRunnable tr = new TransferRunnable(b, i, INITIAL_BALANCE);
+            Thread t = new Thread(tr);
+            t.start();
+        }
+    }
+}
