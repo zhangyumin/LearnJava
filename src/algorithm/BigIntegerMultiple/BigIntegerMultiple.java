adc2518a59a7e9b7dc30ff1af31c6cd8e45c6c17
zhangyumin618@163.com <zhangyumin618@163.com>
2016-05-22 16:47:55 +0800

    51nod 1027

--- src/algorithm/BigIntegerMultiple/BigIntegerMultiple.java
+++ src/algorithm/BigIntegerMultiple/BigIntegerMultiple.java
@@ -12,6 +12,14 @@ import java.util.Scanner;
  * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
  * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
  * Created by zym on 16-5-22.
+ *
+ * 给出2个大整数A,B，计算A*B的结果。
+ * Input
+ * 第1行：大数A
+ * 第2行：大数B
+ * (A,B的长度 <= 1000，A,B >= 0）
+ * Output
+ * 输出A * B
  */
 public class BigIntegerMultiple {
     public static void main(String[] args) {
