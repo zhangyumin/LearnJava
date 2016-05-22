63040af47adcd8d3aaf16c770d74bf4b942752d9
zhangyumin618@163.com <zhangyumin618@163.com>
2016-05-22 16:41:09 +0800

    51nod 1027

---
+++ src/algorithm/BigIntegerMultiple/BigIntegerMultiple.java
@@ -0,0 +1,23 @@
+package algorithm.BigIntegerMultiple;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by zym on 16-5-22.
+ */
+public class BigIntegerMultiple {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+        System.out.println(a.multiply(b));
+    }
+}
