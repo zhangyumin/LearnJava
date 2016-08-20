abc6713cef952c7114fe6845e964dfaf43bc1c5c
zhangyumin <zhangyumin618@163.com>
2016-08-20 18:32:52 +0800

    Node

---
+++ src/io/github/zhangyumin/Node/Node.java
@@ -0,0 +1,25 @@
+package io.github.zhangyumin.Node;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by workstation on 16-7-17.
+ */
+public class Node {
+    protected Node next;
+    public int data;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public void display(){
+        System.out.println(data + " ");
+    }
+}
+
