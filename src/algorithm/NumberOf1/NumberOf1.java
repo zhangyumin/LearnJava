a2a5d95e8e175f53cbb3ecc0f21412ed80525056
zhangyumin <zhangyumin618@163.com>
2016-09-03 16:01:15 +0800

    number of 1

---
+++ src/algorithm/NumberOf1/NumberOf1.java
@@ -0,0 +1,29 @@
+package algorithm.NumberOf1;
+
+/**
+ * //
+ * //   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * // ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * // ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * // ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * // ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * //  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * Created by joker on 16-9-3.
+ */
+public class NumberOf1 {
+    public int NumberOf1(int n) {
+    int sum = 0;
+    int num = 1;
+    while(num != 0){
+        if((n & num) == num){
+            sum ++;
+        }
+        num = num << 1;
+    }
+    return sum;
+}
+
+    public static void main(String[] args) {
+        System.out.println(new NumberOf1().NumberOf1(-2147483648));
+    }
+}
