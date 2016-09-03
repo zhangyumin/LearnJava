313bef20bc1199cfd39a6ccebec296d3174c0786
zhangyumin <zhangyumin618@163.com>
2016-09-03 23:12:11 +0800

    has sub-tree

--- src/algorithm/HasSubtree/HasSubtree.java
+++ src/algorithm/HasSubtree/HasSubtree.java
@@ -38,6 +38,12 @@ public class HasSubtree {
     }
 
     public boolean isSame(TreeNode root1, TreeNode root2){
+        if(root2 == null){
+            return true;
+        }
+        if(root1 == null){
+            return false;
+        }
         if(root1.val == root2.val){
             return isSame(root1.left, root2.left) && isSame(root1.right, root2.right);
         }
